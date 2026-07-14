9cd0624ae62525a1b1c200582adb1078b43ee41f
abbeyzerotheorem <olamilekansunday445@gmail.com>
2026-07-14 14:50:51 +0200

    Remove .next/ from tracking (should be gitignored)

--- .next/dev/static/chunks/app_page_tsx_0ok9jah._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0mmn3zu._.js","static/chunks/node_modules_next_0jbgsu5._.js","static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_0q79z23._.js","static/chunks/node_modules_motion-dom_dist_es_0g6cuzy._.js","static/chunks/node_modules_framer-motion_dist_es_1xxi2pp._.js","static/chunks/node_modules_0xasco_._.js"],
-    source: "dynamic"
-});
